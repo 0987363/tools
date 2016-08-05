ab9626ee2be4d026824550355fce91c148289425
0987363 <0987363@gmail.com>
2016-08-05 11:28:35 +0800

    add format

---
+++ format_code.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python2.7
+
+import os, sys, re
+
+
+def splitFile(file, names):
+    if re.match('^\.', file) :
+        return
+
+    path = dir + file
+    if os.path.isdir(path):
+        return
+    if file == 'Thumbs.db':
+        return
+
+    file = file.upper().strip()
+    m = re.match('^([A-Z]+)-?(\d+).+\.([a-zA-Z]+)$', file)
+    if not m :
+        return
+    name, code, t = m.groups()
+    if len(code) > 3:
+        if code[:1] == '0':
+            code = code[-3:]
+    newName = dir + name + '-' + code + '.' + t
+
+    if not names.get(newName) :
+        names[newName] = []
+    names[newName].append(path)
+
+    print name + '-' + code, ":", file
+
+def start(names):
+    for key, value in names.items() :
+        print 'mkdir:', key
+        os.system('mkdir "' + key + '"')
+        for item in value :
+            print 'mv', item
+            os.system('mv "' + item + '" "' + key + '"')
+
+
+for dir in sys.argv:
+    if dir == sys.argv[0]:
+        continue
+
+    names = {}
+    if os.path.split(dir)[1] != '':
+        dir = dir + '/'
+
+    list = os.listdir(dir)
+
+    for file in list:
+        print file
+        splitFile(file, names)
+
+    start(names)
